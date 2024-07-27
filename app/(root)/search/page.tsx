c8d2a7d97c303ecd52ddd983fe2eb6109759b03f
SiddharthJiyani <sidjiyani2003@gmail.com>
2024-07-27 13:10:02 +0530

    communities and search implemented

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,12 +1,11 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-// import UserCard from "@/components/cards/UserCard";
-// import Searchbar from "@/components/shared/Searchbar";
-// import Pagination from "@/components/shared/Pagination";
+import Pagination from "@/components/shared/Pagination";
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
 
 async function Page({
   searchParams,
@@ -29,7 +28,7 @@ async function Page({
         <section>
             <h1 className="head-text mb-10">Search</h1>
 
-            {/* Search bar */}
+            <Searchbar routeType='search' />
 
             <div className="mt-14 flex flex-col gap-9">
               {
@@ -53,6 +52,12 @@ async function Page({
                 )
               }
             </div>
+
+            <Pagination
+              path='search'
+              pageNumber={searchParams?.page ? +searchParams.page : 1}
+              isNext={result.isNext}
+            />
         </section>
     )
 }
